d7a8035e6433d3543b577e725a2add9df94119d3
Ismail Erdogan <erdogan.isma@gmail.com>
2025-10-15 12:47:55 +0200

    enthält Zahl implementiert.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,20 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static String password = "Testpasswort";
+    static String password = "Test7passwort";
     static int minPasswordLength = 8;
+    static int[] digits = {0,1,2,3,4,5,6,7,8,9};
+
 
 
     public static void main(String[] args) {
         System.out.println("dasPasswortWirdValidiert");
         passwordLengthValidation(password);
+   //   passwordContainsDigits(password);
+   //   passwordContainsDigits(password);
+        boolean test = passwordContainsDigits(password);
+        System.out.println(test);
+
 
 
         }
@@ -21,6 +28,29 @@ public class Main {
         }else{
             return false;
         }
+
+     }
+
+     public static Boolean passwordContainsDigits(String password){
+        boolean status = false;
+
+        for(int i=0;i <password.length();i++){
+            char a = password.charAt(i);
+            if(Character.isDigit(a)){
+                System.out.println(a);
+                int numberOfCharA = Character.getNumericValue(a);
+                for(int x=0; x<digits.length;x++){
+                    if(digits[x] == numberOfCharA){
+                        System.out.println("innereschleife");
+                        status = true;
+                    }
+                }
+            }
+
+
+                    }
+
+        return status;
 
      }
 
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -24,15 +24,37 @@ class MainTest {
             //Given
             String password = "Hallo";
             boolean expected = false;
-
             //When
             boolean actual = Main.passwordLengthValidation(password);
+            //Then
+            assertFalse(actual);
+        }
+
+        @Test
+        void passwordContainsDigitsTest(){
+            //Given
+            String password = "Ha8l7l546oWelt8";
+            boolean expected = true;
+            //When
+            boolean actual = Main.passwordContainsDigits(password);
 
             //Then
-
-            assertFalse(actual);
-
-
+            assertTrue(actual);
         }
+
+         @Test
+         void passwordContainsDigitsTestFalse(){
+            //Given
+            String password = "HalloWelt";
+            boolean expected = false;
+            //When
+            boolean actual = Main.passwordContainsDigits(password);
+
+            //Then
+            assertFalse(actual);
+    }
+
+
+
 
 }
