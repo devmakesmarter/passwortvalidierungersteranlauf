c973f07bda13481e1b4da8812c8979b7d7ab7339
Ismail Erdogan <erdogan.isma@gmail.com>
2025-10-15 10:33:28 +0200

    Passwordlänge implementiert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,24 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static String password = "Testpasswort";
+    static int minPasswordLength = 8;
+
+
     public static void main(String[] args) {
         System.out.println("dasPasswortWirdValidiert");
-        String password = "hahaah";
-        ersteMethode(password);
+        passwordLengthValidation(password);
+
 
         }
 
 
-     public static void ersteMethode(String password){
-        System.out.println(password);
+     public static Boolean passwordLengthValidation(String password){
+        if(password.length() > minPasswordLength){
+            return true;
+        }else{
+            return false;
+        }
 
      }
 
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -8,11 +8,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
         @Test
-        void test(){
-            System.out.println("test");
+        void passwordLengthValidationTest(){
+        //Given
+            String password ="HalloPasswort";
+            boolean expected = true;
+        //When
+            boolean actual = Main.passwordLengthValidation(password);
+        //Then
+            assertEquals(expected,actual);
+            assertTrue(actual);
+        }
+
+        @Test
+        void passwordLengthValidationsTestFalse(){
+            //Given
+            String password = "Hallo";
+            boolean expected = false;
+
+            //When
+            boolean actual = Main.passwordLengthValidation(password);
+
+            //Then
+
+            assertFalse(actual);
 
 
         }
-
 
 }
